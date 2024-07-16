9167352ae64d261af3f02ca121be36fa14e91f45
Piotr Narajowski <piotr.narajowski@codecoup.pl>
2024-07-16 13:43:57 +0200

    patch 16
    patch 16

--- .github/check_for_changes.py
+++ .github/check_for_changes.py
@@ -26,10 +26,16 @@ def main():
         return
 
     try:
-        # commit = "HEAD"
-        # upstream = "origin/master"
-        # mb = run_cmd(f"git merge-base {upstream} {commit}")
-        # upstream = mb[0]
+        commit = "HEAD"
+        upstream = "origin/master"
+        mb = run_cmd(f"git merge-base {upstream} {commit}")
+        upstream = mb[0]
+        remote = subprocess.run(
+            ['git', 'remote', '-v'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True
+        )
+
+        print(f"Git remote -v: {remote}")
 
         result = subprocess.run(
             ['git', 'diff', '--name-only', 'HEAD^', 'HEAD', '--', 'autopts/wid/'],
@@ -41,12 +47,6 @@ def main():
             stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True
         )
 
-        remote = subprocess.run(
-            ['git', 'remote', '-v'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True
-        )
-
-        print(f"Git remote -v: {remote}")
         print(f"Rev parse result: HEAD, HEAD^, master. origin/master {rev_parse}")
         print(f"git diff output: {result.stdout}")
 
